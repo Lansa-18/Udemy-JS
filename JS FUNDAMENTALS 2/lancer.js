fec0efa4f385f22feb02a988b9d6fa3ae265e1fb
GentleMizt <olamideiyanda18@gmail.com>
2023-03-12 21:56:42 +0100

    progress

---
+++ JS FUNDAMENTALS 2/lancer.js
@@ -0,0 +1,12 @@
+    'use strict'; // Helps us write cleaner codes and avoid bug errors
+    
+    let hasDriversLicense = false;
+    const passTest = true;
+
+    if(passTest) hasDriversLicense = true;
+    if(hasDriversLicense) console.log('I can drive :D');
+
+    const interface = 'Audio'
+
+
+
