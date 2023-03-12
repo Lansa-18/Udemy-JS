9f5683fdb5e40966b2fb5e87674e6ce67db47a0c
GentleMizt <olamideiyanda18@gmail.com>
2023-03-12 20:13:56 +0100

    initial commit

---
+++ lancer.js
@@ -0,0 +1,18 @@
+    // CODING CHALLENGE 4.
+
+    const billValue = Number(prompt('Enter your bill'));
+    let tip = 0.2;
+    let tipValue;
+
+    // if (billValue >= 50 && billValue <= 300) {
+    //     let tip = 0.15;
+    //     tipValue = billValue * tip
+    //     console.log(`The bill was ${billValue}, and the total value ${billValue + tipValue}`);
+    // } else {
+    //     tipValue = billValue * tip
+    //     console.log(`The bill was ${billValue}, and the total value ${billValue + tipValue}`);
+    // }
+
+    let gettingTip = billValue >= 50 && billValue <= 300 ? tipValue = billValue * 0.15 : tipValue = billValue * tip;
+    console.log(`The bill was ${billValue}, and the total value ${billValue + tipValue}`);
+
