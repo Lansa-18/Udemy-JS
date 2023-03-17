aeb536dd88764ee6407f2ae075263e19353b8ebf
GentleMizt <olamideiyanda18@gmail.com>
2023-03-17 01:20:48 +0100

    progress

--- JS FUNDAMENTALS 2/lancer.js
+++ JS FUNDAMENTALS 2/lancer.js
@@ -431,12 +431,53 @@
     // While loops are the kind of loops that are used for cases where we dont know how many times the loop is going to run
     // In a case where by we do know the amount of times to iterate or Loop over, a For Loop is to be used.
 
-    let dice = Math.trunc(Math.random() * 6) + 1;
+    // let dice = Math.trunc(Math.random() * 6) + 1;
 
-    while (dice !== 6) {
-        console.log(`You rolled a ${dice}`);
-        dice = Math.trunc(Math.random() * 6) + 1;
-        if (dice === 6) {
-            console.log(`The loop is about to end`);
-        }
+    // while (dice !== 6) {
+    //     console.log(`You rolled a ${dice}`);
+    //     dice = Math.trunc(Math.random() * 6) + 1;
+    //     if (dice === 6) {
+    //         console.log(`The loop is about to end`);
+    //     }
+    // }
+// CODING CHALLENGE 4
+
+
+    // const bills = [125, 555, 44];
+    // const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+    // const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+    const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+    let tips = [];
+    let totals = [];
+    let tipValue;
+
+
+    function calcTip (billValue) {
+        return billValue >= 50 && billValue <= 300 ? tipValue = billValue * 0.15 : tipValue = billValue * 0.2;
     }
+
+    for (let i = 0; i < bills.length; i++) {
+        const tip = calcTip(bills[i]);
+        tips.push(tip);
+        totals.push(bills[i] + tip);
+    }
+
+    console.log(bills, tips, totals);
+
+    function calcAverage (arr){
+        let sum = 0;
+        let avg = 0;
+        for (let i = 0; i < arr.length; i++){
+            sum += (arr[i]);
+            avg = sum / arr.length;
+        }
+        console.log(sum);
+        console.log(avg);
+    }
+
+    calcAverage(totals);
+
+
+
+
