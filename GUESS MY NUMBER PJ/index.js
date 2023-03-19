828fd24a7d2bf3890c70e9a9e749bf845441ca26
GentleMizt <olamideiyanda18@gmail.com>
2023-03-19 20:27:17 +0100

    progress

--- GUESS MY NUMBER PJ/index.js
+++ GUESS MY NUMBER PJ/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
-
-
 console.log(document.querySelector('.message').textContent);
+
+// API stands for Application Programming Interface
+// APIs are libraries that are written in Javascript and we can actually use them in out JS.
