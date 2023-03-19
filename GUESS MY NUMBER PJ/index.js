bae9d8b3f9b32a41bb2a76437bd07efdbb37dbb6
GentleMizt <olamideiyanda18@gmail.com>
2023-03-19 20:40:37 +0100

    progress

--- GUESS MY NUMBER PJ/index.js
+++ GUESS MY NUMBER PJ/index.js
@@ -4,3 +4,12 @@ console.log(document.querySelector('.message').textContent);
 
 // API stands for Application Programming Interface
 // APIs are libraries that are written in Javascript and we can actually use them in out JS.
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 19;
+
+// With an input field, to get the value property we have to use the '.value' method
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
