e1222fd87737268c489caea81af69a3a95d0c924
GentleMizt <olamideiyanda18@gmail.com>
2023-03-19 20:49:06 +0100

    progress

--- GUESS MY NUMBER PJ/index.js
+++ GUESS MY NUMBER PJ/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
 // API stands for Application Programming Interface
@@ -13,3 +13,16 @@ document.querySelector('.score').textContent = 19;
 // With an input field, to get the value property we have to use the '.value' method
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+// An event is something that happens on the page, an event listner waits for the certain event to happen on the page and then reacts to it.
+
+let btnCheck = document.querySelector('.check');
+
+// Adding an event listner
+// the function that is passed into an event listner is called an event handler
+btnCheck.addEventListener('click', () =>{
+    console.log(document.querySelector('.guess').value);
+});
+
+// this particular function will only be called anytime that event takes place and that is the clicking of the button
