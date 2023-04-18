b7b4453c4d8318140f12b78ec06ccc8b02e3459c
GentleMizt <olamideiyanda18@gmail.com>
2023-04-18 22:05:27 +0100

    progress

---
+++ 07-BTC-JAVASCRIPT/lancer.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// JAVASCRIPT is a:
+// - high-level
+// - garbage-collected
+// - Interpreted or Just in time Compiled
+// - multi-paradigm :- Three Optimal paradigms are;
+    // - Procedural Programming
+    // - Object Oriented Programming (OOP)
+    // - Functional Programming (FP)
+// - prototype based object oriented
+// - first class functions
+// - dynamic
+// - single-threaded
+// - non-blocking event loop
+
