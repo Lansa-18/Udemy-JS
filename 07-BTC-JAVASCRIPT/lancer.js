3562befc4652d61397b1a285f8bee9128cd280a8
GentleMizt <olamideiyanda18@gmail.com>
2023-04-18 22:13:26 +0100

    progress

--- 07-BTC-JAVASCRIPT/lancer.js
+++ 07-BTC-JAVASCRIPT/lancer.js
@@ -9,8 +9,11 @@
     // - Object Oriented Programming (OOP)
     // - Functional Programming (FP)
 // - prototype based object oriented
-// - first class functions
+// - first class functions i.e functions are simply treated as variables
 // - dynamic
 // - single-threaded
 // - non-blocking event loop
+
+// CONCURRENCY MODEL basically means how the javascript engine handles multiple tasks happening at the same time.The reason we use thus model is bcuz js in single threaded.
+// It is done using event loops i.e taking long running tasks, execute them in the background, and puts them back in the thread once they are finished.
 
