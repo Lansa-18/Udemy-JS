516669aeb52c68a1ed71e9f178903ab8c4416539
GentleMizt <olamideiyanda18@gmail.com>
2023-05-14 10:04:36 +0100

    progress

--- 07-BTC-JAVASCRIPT/lancer.js
+++ 07-BTC-JAVASCRIPT/lancer.js
@@ -204,12 +204,26 @@ const lancer = {
   firstName: 'Lancer',
   year: 2004,
   calcAge: function () {
-    console.log(this);
+    // console.log(this);
     console.log(2037 - this.year);
+
+    const self = this; // A way we use in preserving the 'this' keyword
+
+    const isMillenial = function () {
+      // Now it is accessible in this function
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996);
+
+      //   console.log(this);
+      //   console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
-  greet: () => console.log(`Hey ${this.firstName}`),
+
+  greet: () => {
+    console.log(`Hey ${this.firstName}`);
+  },
 };
 
-lancer.greet();
-
-
+// lancer.greet();
+lancer.calcAge();
