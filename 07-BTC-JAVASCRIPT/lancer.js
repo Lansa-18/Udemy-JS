1ab508d59a75ec882a53effdb1dcf48f2c2279c5
GentleMizt <olamideiyanda18@gmail.com>
2023-05-14 10:23:32 +0100

    progress

--- 07-BTC-JAVASCRIPT/lancer.js
+++ 07-BTC-JAVASCRIPT/lancer.js
@@ -248,7 +248,14 @@ const addArr = (a, b) => {
 
 // addArr(2,5);
 
+let age = 30;
+let oldAge = age;
+age = 31;
 
+console.log(age);
+console.log(oldAge);
 
-
-
+const me = {
+  name: 'lancer',
+  age: 30,
+};
