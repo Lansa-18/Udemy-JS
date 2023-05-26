9c2a6f1cde09f730b8beb1336ccf0bf64239ba53
GentleMizt <olamideiyanda18@gmail.com>
2023-05-26 19:00:04 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -29,6 +29,15 @@ const restaurant = {
 
 // OBJECT DESTRUCTURING
 const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {name: restaurantName,
+     openingHours: hours,
+      categories:
+       tags
+    } = restaurant;
+console.log(restaurantName, hours, tags);
+
 
 
 
