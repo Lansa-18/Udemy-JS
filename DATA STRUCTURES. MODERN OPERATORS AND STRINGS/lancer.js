8c2fba215299d0c9377d3ab2047e6eca868905b4
GentleMizt <olamideiyanda18@gmail.com>
2023-05-26 19:46:04 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -48,6 +48,11 @@ const obj = {a: 23, b: 7, c: 14};
 ({a,b} = obj); // The code was wrapped in parenthesis because when beginning a javascript line with curly brackets, it expects a code block
 console.log(a,b);
 
+// NESTED OBJECTS DESTRUCTURING
+const {fri} = openingHours.fri;
+
+console.log(fri);
+
 
 
 
