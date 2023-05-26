5d76a5b911a29cfcb3d357e4ba9dbc131176905d
GentleMizt <olamideiyanda18@gmail.com>
2023-05-26 18:11:04 +0100

    progress

---
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// DESTRUCTURING ARRAYS
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
