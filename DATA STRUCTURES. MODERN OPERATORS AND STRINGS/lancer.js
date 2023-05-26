29bf8fbd8477ccc57125d61d93c0a731d6f3e1c5
GentleMizt <olamideiyanda18@gmail.com>
2023-05-26 18:12:10 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -9,7 +9,12 @@ const restaurant = {
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 };
 
+// Retrieving elements normally
 const arr = [2,3,4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
+
+//Destructuring the array
+const [x,y,z] = arr;
+console.log(x,y,z);
