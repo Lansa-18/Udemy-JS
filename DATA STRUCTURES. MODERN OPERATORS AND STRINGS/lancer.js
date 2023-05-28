984a64df506db8bbccca330d5cb01836065392dd
GentleMizt <olamideiyanda18@gmail.com>
2023-05-28 18:09:40 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -261,7 +261,7 @@ const guestCorrect = restaurant.numGuests ?? 10;
 /////////////////// --------------- LOGICAL ASSIGNMENT OPERATOR -------------------- //////////////////////
 const rest1 = {
   name: 'Capri',
-//   numGuests: 20,
+  //   numGuests: 20,
   numGuests: 0,
 };
 
@@ -289,72 +289,70 @@ rest2.owner &&= '<ANONYMOUS>';
 // console.log(rest1);
 // console.log(rest2);
 
-
 /////////// ------------- CODING CHALLENGE #1 ---------------- //////////////////
 // GIVEN DATA
 
 const game = {
-    team1: 'Bayern Munich',
-    team2: 'Borrussia Dortmund',
-    players: [
-      [
-        'Neuer',
-        'Pavard',
-        'Martinez',
-        'Alaba',
-        'Davies',
-        'Kimmich',
-        'Goretzka',
-        'Coman',
-        'Muller',
-        'Gnarby',
-        'Lewandowski',
-      ],
-      [
-        'Burki',
-        'Schulz',
-        'Hummels',
-        'Akanji',
-        'Hakimi',
-        'Weigl',
-        'Witsel',
-        'Hazard',
-        'Brandt',
-        'Sancho',
-        'Gotze',
-      ],
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
     ],
-    score: '4:0',
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-    date: 'Nov 9th, 2037',
-    odds: {
-      team1: 1.33,
-      x: 3.25,
-      team2: 6.5,
-    },
-  };
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
-  // 1. Create 1 player array for each team (variables 'players1' and 'players2').
-  const [players1, players2] = game.players;
+// 1. Create 1 player array for each team (variables 'players1' and 'players2').
+const [players1, players2] = game.players;
 
-  // 2. 
-  const [gk, ...fieldPlayers] = players1;
-  
-  // 3. 
-  const allPlayers = [...players1, ...players2];
+// 2.
+const [gk, ...fieldPlayers] = players1;
 
-  // 4. 
-  const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// 3.
+const allPlayers = [...players1, ...players2];
 
-  // 5.
-  const {team1, x:draw, team2} = game.odds;
+// 4.
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
-  // 6. 
-  const printGoals = function(...players){
-    console.log(`${players} scored goals`);
-    console.log(`${players.length} goals were scored`);
+// 5.
+const { team1, x: draw, team2 } = game.odds;
 
-  }
+// 6.
+const printGoals = function (...players) {
+  console.log(`${players} scored goals`);
+  console.log(`${players.length} goals were scored`);
+};
 
-  printGoals('Lewandoskie', 'Davies', 'Muller', 'Kimmich');
-  printGoals(...game.scored);
+printGoals('Lewandoskie', 'Davies', 'Muller', 'Kimmich');
+printGoals(...game.scored);
