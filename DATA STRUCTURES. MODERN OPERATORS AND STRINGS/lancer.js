caf7c4e39d76620fc705709c6bcb392bd08f8f23
GentleMizt <olamideiyanda18@gmail.com>
2023-05-28 16:39:08 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -273,8 +273,12 @@ const rest2 = {
 // OR assignment operator
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// NULLISH COALESCING operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
 
 console.log(rest1);
 console.log(rest2);
