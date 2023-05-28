dc18090c2853616e497f88e5b4933f6f5a5b73d1
GentleMizt <olamideiyanda18@gmail.com>
2023-05-28 16:08:01 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -223,32 +223,38 @@ add(...x);
 // PROPERTIES OF LOGICAL OPERATORS
 // They USE any data type, RETURN any data type and short-circuiting.
 console.log('----------- OR ------------------- ');
-console.log(3 || 'lancer');
-console.log('' || 'lancer');
-console.log(true || 0);
-console.log(undefined || null); // Undefined and null are both falsy values, but because the first operand was false, it automatically returned the second operand which is 'null', even though it is also a falsy value.
+// console.log(3 || 'lancer');
+// console.log('' || 'lancer');
+// console.log(true || 0);
+// console.log(undefined || null); // Undefined and null are both falsy values, but because the first operand was false, it automatically returned the second operand which is 'null', even though it is also a falsy value.
 
-console.log(undefined || 0 || '' || 'hello' || 23); // In this scenario, 'hello' gets outputed because, all the other operands b4 it were false, and the moment it met the condition (truthy value), it got short-circuited and it got outputed.
+// console.log(undefined || 0 || '' || 'hello' || 23); // In this scenario, 'hello' gets outputed because, all the other operands b4 it were false, and the moment it met the condition (truthy value), it got short-circuited and it got outputed.
 
 // Application of Short-circuiting in the || (OR) operator
-restaurant.numGuests = 40;
+// restaurant.numGuests = 40;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;  // TERNARY OPERATOR
+// console.log(guests1);
+
+// const guest2 = restaurant.numGuests || 10;
+// console.log(guest2);
+
+
+// console.log('----------- AND ------------------- ');
+// console.log(0 && 'jonas');
+// console.log(7 && 'jonas');
+// console.log(12 && 'lancer' && true && undefined && 23);
+
+// Apllication  of Short-circuiting in the && (AND) operator
+// if(restaurant.orderPizza){
+//     restaurant.orderPizza('Mushrooms', 'Chicken');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('Chicken', 'Spinach')
+
+// THE NULLISH COALESCING OPERATOR(??)restaurant.numGuests = 40;
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;  // TERNARY OPERATOR
 console.log(guests1);
 
 const guest2 = restaurant.numGuests || 10;
 console.log(guest2);
 
-
-console.log('----------- AND ------------------- ');
-console.log(0 && 'jonas');
-console.log(7 && 'jonas');
-console.log(12 && 'lancer' && true && undefined && 23);
-
-// Apllication  of Short-circuiting in the && (AND) operator
-if(restaurant.orderPizza){
-    restaurant.orderPizza('Mushrooms', 'Chicken');
-}
-
-restaurant.orderPizza && restaurant.orderPizza('Chicken', 'Spinach')
-
-// THE NULLISH COALESCING OPERATOR(??)
