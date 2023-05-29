40b68961b416baff46975f55637ff5f119d222c3
GentleMizt <olamideiyanda18@gmail.com>
2023-05-29 16:40:22 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -423,8 +423,10 @@ const values = Object.values(openingHours);
 
 // To loop over the entire object, we need to make use of the entries. ENTRIES contain both the Keys and the values.
 const entries = Object.entries(openingHours);
-console.log(entries);
+// console.log(entries);
 
 for (const [day, {open, close}] of entries){
-    console.log(`On ${day} we open ${open} and close at ${close}`);
+    // console.log(`On ${day} we open ${open} and close at ${close}`);
 }
+
+
