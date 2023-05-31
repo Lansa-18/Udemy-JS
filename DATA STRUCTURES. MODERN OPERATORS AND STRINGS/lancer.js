c3ff2f8ee97f8605621f6a0cc947a106588752a3
GentleMizt <olamideiyanda18@gmail.com>
2023-05-31 18:28:04 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -550,3 +550,4 @@ console.log(rest.size);
 // Getting data from Maps if the keys are ARRAY BASED
 // In order for this to work, the array needs to be assigned to a variable, that way the variable is the one storing that array in memory. Then when defining the key, we access the array based on that variable name, same way if we want to access the value that was assigned to that particular key(i.e the array), we use the same variable name to access it. THE SAME PRINCIPLE APPLIES TO OBJECTS.
 console.log(rest.get(mapArr));
+
