3aea9e174b7148b80c8cc81a1486c3be14c25b62
GentleMizt <olamideiyanda18@gmail.com>
2023-05-31 17:57:14 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -516,7 +516,7 @@ console.log(rest.set(2, 'Lisbon Portugal'));
 
 rest
   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('Open', 11)
+  .set('open', 11)
   .set('close', 23)
   .set(true, 'We are Open :D ')
   .set(false, 'We are Closed :( ');
@@ -524,3 +524,7 @@ rest
 // In Order to read data from Maps, we use the get method
 console.log(rest.get('name'));
 console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
