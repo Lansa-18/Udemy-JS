e64a98f943939313db6e2dccc13423ae74804284
GentleMizt <olamideiyanda18@gmail.com>
2023-06-03 19:08:06 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -822,6 +822,7 @@ const convertCamelCase = function () {
 
 button.addEventListener('click', convertCamelCase);
 
+// JONAZ' SOLUTION
 // button.addEventListener('click', function () {
 //   const inputText = input.value;
 //   const rows = inputText.split('\n');
@@ -836,3 +837,19 @@ button.addEventListener('click', convertCamelCase);
 //     input.value = '';
 //   }
 // });
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const eachFlight = flights.split('+');
+
+for (const flight of eachFlight){
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴': '' }${type.replaceAll('_', ' ')} ${from} ${to} (${time.replace(':','h')})`;
+  console.log(output);
+}
