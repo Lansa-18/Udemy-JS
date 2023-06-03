94196acfb25108fe7d99ea82a4cc353c3c129a51
GentleMizt <olamideiyanda18@gmail.com>
2023-06-03 18:32:13 +0100

    progress

--- DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
+++ DATA STRUCTURES. MODERN OPERATORS AND STRINGS/lancer.js
@@ -827,13 +827,17 @@ const button = document.querySelector('button');
 
 // button.addEventListener('click', convertCamelCase);
 
-button.addEventListener('click', function(){
-  const inputText = input.value; 
+button.addEventListener('click', function () {
+  const inputText = input.value;
   const rows = inputText.split('\n');
   console.log(rows);
-  for (const [i,row] of rows.entries()){
-    const [first,second] = row.toLowerCase().trim().split('_')
-    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
-    console.log(`${output.padEnd(20)}${'✅'.repeat(i+1)}`);
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+    input.value = '';
   }
-})
+});
