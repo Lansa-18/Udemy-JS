a51fd687227e47428f20772398ec6822b26d45b1
GentleMizt <olamideiyanda18@gmail.com>
2023-06-06 21:47:11 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -4,6 +4,9 @@
 // without default parameters
 const bookingArr = []
 const createBooking = function(flightNum, numPassengers, price){
+    numPassengers = numPassengers || 1;
+    price = price || 199;
+    
     const booking = {
         flightNum,
         numPassengers,
