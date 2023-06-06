46ea7f05d9132b7ff77e51f76d586fc2201bbcfa
GentleMizt <olamideiyanda18@gmail.com>
2023-06-06 21:55:04 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -1,24 +1,26 @@
 'use strict';
 ////////////////////////////////////// DEFAULT PARAMETERS //////////////////////////////
 
+const bookingArr = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5 WAY OF DOING IT
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
 
-const bookingArr = []
-const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers){
-    //ES5 WAY OF DOING IT
-    // numPassengers = numPassengers || 1;
-    // price = price || 199;
-
-
-    const booking = {
-        flightNum,
-        numPassengers,
-        price
-    }
-    console.log(booking);
-    bookingArr.push(booking)
-}
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookingArr.push(booking);
+};
 // To skip an argument so that we can leave it at it's default parameter that was assigned, all we need to do is set it to undefined
-createBooking('ABC12', 4, undefined)
+createBooking('ABC12', 4, undefined);
 createBooking('LH123');
 createBooking('LK123', 5, 1200);
 createBooking('AK123', 2);
