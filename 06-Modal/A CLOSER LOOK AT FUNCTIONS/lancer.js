ed5af612fb4dc8220e5ec8f328abdb18af29e764
GentleMizt <olamideiyanda18@gmail.com>
2023-06-13 20:34:01 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -198,4 +198,5 @@ const poll = {
   question: 'What is your favorite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   
+  answers: new Array(4).fill(0),
 }
