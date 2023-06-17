ad1c3b03f0e068f1163215073e6961708afbcb0d
GentleMizt <olamideiyanda18@gmail.com>
2023-06-17 20:53:48 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -245,40 +245,40 @@ const addVat = add(0.23);
 // };
 
 // JONAS' SOLUTION.
-const poll = {
-  question: 'What is your favorite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+// const poll = {
+//   question: 'What is your favorite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
 
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    // Getting the answer
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write Option Number)`
-      )
-    );
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     // Getting the answer
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write Option Number)`
+//       )
+//     );
 
-    // Registering the answer
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
+//     // Registering the answer
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
 
-    this.displayResults();
-    this.displayResults('string');
-  },
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
 
-  displayResults(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      console.log(`Poll results are ${this.answers.join(', ')}`);
-    }
-  },
-};
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
 
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-  poll.displayResults.call({answers: [5,2,3]}, 'string')
-  poll.displayResults.call({answers: [5,2,3,9,6,1]}, 'string')
+//   poll.displayResults.call({answers: [5,2,3]}, 'string')
+//   poll.displayResults.call({answers: [5,2,3,9,6,1]}, 'string')
