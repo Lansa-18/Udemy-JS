41ea32c3e9eab95f8715e6251f2185ca379c38f3
GentleMizt <olamideiyanda18@gmail.com>
2023-06-17 18:50:50 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -251,4 +251,7 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
 
   answers: new Array(4).fill(0),
+  registerNewAnswer(){
+    const answer = Number (prompt(`${this.question}\n${this.options.join('\n')}\n(Write Option Number)`));
+  }
 };
