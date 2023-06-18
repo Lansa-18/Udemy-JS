6582409740f064dda3a9153eeae5c113294c5753
GentleMizt <olamideiyanda18@gmail.com>
2023-06-18 22:15:24 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -279,4 +279,3 @@ pollBtn.addEventListener('click', poll.registerNewAnswers.bind(poll));
 
 //   poll.displayResults.call({answers: [5,2,3]}, 'string')
 //   poll.displayResults.call({answers: [5,2,3,9,6,1]}, 'string')
-
