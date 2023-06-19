d22c66f455c83f957e8bc747eb822b1c31923b81
GentleMizt <olamideiyanda18@gmail.com>
2023-06-19 16:19:54 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -322,3 +322,5 @@ booker();
 // A closure is the closed over VARIABLE ENVIRONMENT of the execution context in which a function was created, even after the execution context is gone.
 // A closure gives a function access to all the variables of it's PARENT FUNCTION, even after that parent function has returned. The function keeps a reference to it's outer scop, which preserves the scope chain throughout time.
 // A closure makes sure that a function doesn't loose connection to variables that existed at the function's birth place.
+
+// A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables that were present in the environment where thr function was created.
