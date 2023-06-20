ba26a59e72b0add4def80c63c5d0606d97656a8c
GentleMizt <olamideiyanda18@gmail.com>
2023-06-20 07:57:04 +0100

    progress

--- 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
+++ 06-Modal/A CLOSER LOOK AT FUNCTIONS/lancer.js
@@ -363,6 +363,7 @@ console.dir(f);
 const boardPassengers = function(n, wait){
   const perGroup = n/3;
 
+  setTimeout(function(){}, 1000)
 
   console.log(`Will start boarding in ${wait} seconds`);
 }
