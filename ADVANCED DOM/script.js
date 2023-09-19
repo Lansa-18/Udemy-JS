e9a7d22e6965f28f889223a7e7d8456276d1319e
Mapelujo <olamideiyanda18@gmail.com>
2023-09-19 22:23:08 +0100

    progress

--- ADVANCED DOM/script.js
+++ ADVANCED DOM/script.js
@@ -10,7 +10,7 @@ const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('nav');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');  
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////// FUNCTIONS /////////////////////////////
 
@@ -129,35 +129,34 @@ tabsContainer.addEventListener('click', e => {
     .classList.add('operations__content--active');
 });
 
-
 //   MENU FADE ANIMATION
-nav.addEventListener('mouseover',function(e){
+nav.addEventListener('mouseover', function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
-    siblings.forEach((el => {
+    siblings.forEach(el => {
       if (el !== link) el.style.opacity = 0.5;
-    }))
+    });
     logo.style.opacity = 0.5;
   }
 });
 
-nav.addEventListener('mouseout',function(e){
+nav.addEventListener('mouseout', function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
-    siblings.forEach((el => {
+    siblings.forEach(el => {
       if (el !== link) el.style.opacity = 1;
-    }))
+    });
     logo.style.opacity = 1;
   }
 });
 
-// STICKY NAVIGATION 
+// STICKY NAVIGATION
 // const initialCoords = section1.getBoundingClientRect();
 // console.log(initialCoords);
 
@@ -172,7 +171,7 @@ nav.addEventListener('mouseout',function(e){
 //   }
 // })
 
-// USING THE INTERSECTION OBSERVER API, A BETTER TOOL 
+// USING THE INTERSECTION OBSERVER API, A BETTER TOOL
 // STICKY NAVIGATION (INTERSECTION OBSERVER API)
 
 // const observerCallBack = (entries, observer) => {
