46edbfb1e726300a9f99c917dc9e06094540ab96
Mapelujo <olamideiyanda18@gmail.com>
2023-10-10 12:38:21 +0100

    progress

--- OOP/script.js
+++ OOP/script.js
@@ -5,3 +5,9 @@ const Person = function(firstName, birthYear){
 }
 
 new Person('Lancer', 2004);
+
+// when a function is called with the (new) keyworrd, 4 things happens behind the scene.
+// 1. A new {} is created
+// 2. function is then calleed, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns the {}
