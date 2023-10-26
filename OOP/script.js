819e3b4069d14cea4c1ef03759599bd7ae74decc
Mapelujo <olamideiyanda18@gmail.com>
2023-10-26 20:59:42 +0100

    progress

--- OOP/script.js
+++ OOP/script.js
@@ -463,6 +463,9 @@ StudentProto.init = function(firstName, birthYear, course){
   this.course = course;
 }
 
+StudentProto.introduce = function(){
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
 const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'MicroBiology');
 
