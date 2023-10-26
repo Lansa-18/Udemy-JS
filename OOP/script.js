a057dd4c68b2d4f2cadc72ea2071cc0ac8349b25
Mapelujo <olamideiyanda18@gmail.com>
2023-10-26 21:10:54 +0100

    progress

--- OOP/script.js
+++ OOP/script.js
@@ -480,5 +480,12 @@ class Account{
     this.owner = owner;
     this.currency = currency;
     this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${owner}!!`);
   }
 }
+
+const acc1 = new Account('Lansa', 'EUR', 2830);
+console.log(acc1);
