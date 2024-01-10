f6f867dcfbe837ef1c45895a4cd043b77fa79065
Mapelujo <olamideiyanda18@gmail.com>
2024-01-11 00:30:32 +0100

    progress

--- ASYNCHRONOUS JS/starter/script.js
+++ ASYNCHRONOUS JS/starter/script.js
@@ -5,7 +5,6 @@ const countriesContainer = document.querySelector('.countries');
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  countriesContainer.style.opacity = 1;
 };
 
 const renderCountry = function (data, className = '') {
@@ -24,7 +23,6 @@ const renderCountry = function (data, className = '') {
   </article>    
     `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 };
 
 ///////////////////////////////////////
@@ -152,7 +150,11 @@ const getCountryData = function (country) {
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
       console.error(`${err} 💥💥💥`);
-    });
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
 };
 
 btn.addEventListener('click', function () {
