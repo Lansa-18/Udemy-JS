a464c2fbcec921dfd21e2e143dd1cb05f338762f
Mapelujo <olamideiyanda18@gmail.com>
2024-01-13 17:00:22 +0100

    progress

--- ASYNCHRONOUS JS/starter/script.js
+++ ASYNCHRONOUS JS/starter/script.js
@@ -464,16 +464,26 @@ GOOD LUCK 😀
 //   })
 //   .catch(err => console.error(err));
 
-
 ////////////////////////////////////////////////
 // CONSUMING PROMISES WITH ASYNC AND AWAIT
 
-const whereAmI = async function(country){
-  const res = await fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`);
-  console.log(res);
+const whereAmI = async function () {
+  // Geolocation
+  const position = await getPosition();
+  const { latitude: lat, longitude: lng } = position.coords;
+
+  // Reverse Geocoding
+  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+  const dataGeo = await resGeo.json();
+  console.log(dataGeo.city, dataGeo.country);
+
+  // Country Data
+  const res = await fetch(
+    `https://countries-api-836d.onrender.com/countries/name/${dataGeo.country}`
+  );
   const data = await res.json();
   console.log(data[0]);
   renderCountry(data[0]);
-}
-whereAmI('Argentina');
+};
+whereAmI();
 console.log('FIRST');
