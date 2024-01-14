332bb9886adf0ed12003bd4f232f45aa5b052332
Mapelujo <olamideiyanda18@gmail.com>
2024-01-14 12:39:33 +0100

    progress

--- ASYNCHRONOUS JS/starter/script.js
+++ ASYNCHRONOUS JS/starter/script.js
@@ -426,21 +426,21 @@ GOOD LUCK 😀
 
 // JONAS SOLUTION
 
-// const createImage = function (imgPath) {
-//   return new Promise(function (resolve, reject) {
-//     const img = document.createElement('img');
-//     img.src = imgPath;
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
 
-//     img.addEventListener('load', function () {
-//       imgContainer.append(img);
-//       resolve(img);
-//     });
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
 
-//     img.addEventListener('error', function () {
-//       reject(new Error('Image not Found'));
-//     });
-//   });
-// };
+    img.addEventListener('error', function () {
+      reject(new Error('Image not Found'));
+    });
+  });
+};
 
 // let currentImg;
 
@@ -596,42 +596,6 @@ GOOD LUCK 😀
 */
 
 /// PART 1
-const createImage = function (imgPath) {
-  return new Promise(function (resolve, reject) {
-    const img = document.createElement('img');
-    img.src = imgPath;
-
-    img.addEventListener('load', function () {
-      imgContainer.append(img);
-      resolve(img);
-    });
-
-    img.addEventListener('error', function () {
-      reject(new Error('Image not Found'));
-    });
-  });
-};
-
-// let currentImg;
-// createImage('img/img-1.jpg')
-//   .then(resImg => {
-//     currentImg = resImg
-//     console.log('Img1 Loaded');
-//     return wait(2);
-//   })
-//   .then(() => {
-//     currentImg.style.display = 'none';
-//     return createImage('img/img-2.jpg')
-//   })
-//   .then(resImg => {
-//     currentImg = resImg;
-//     console.log('Image 2 Loaded');
-//     return wait(2);
-//   })
-//   .then(() => {
-//     currentImg.style.display = 'none'
-//   })
-//   .catch(err => console.error(err));
 
 // MY SOLUTION
 // let currentImg;
@@ -658,21 +622,35 @@ const createImage = function (imgPath) {
 // loadNPause();
 
 // JONAS SOLUTION
-const loadNPause = async function(){
-  try {
-    // Loading the first Image
-    let img = await createImage('img/img-1.jpg')
-    console.log('Image 1 loaded');   
-    await wait(2);
-    img.style.display = 'none';
+// const loadNPause = async function(){
+//   try {
+//     // Loading the first Image
+//     let img = await createImage('img/img-1.jpg')
+//     console.log('Image 1 loaded');   
+//     await wait(2);
+//     img.style.display = 'none';
 
-    // Loading the second Image
-    img = await createImage('img/img-2.jpg')
-    console.log('Image 2 loaded');
-    await wait(2);
-    img.style.display = 'none';
-  } catch(err){
+//     // Loading the second Image
+//     img = await createImage('img/img-2.jpg')
+//     console.log('Image 2 loaded');
+//     await wait(2);
+//     img.style.display = 'none';
+//   } catch(err){
+//     console.error(err);
+//   }
+// }
+// loadNPause();
+
+// PART 2
+const loadAll = async function(imgArr){
+  try{
+    const resImg = await imgArr.map(function(imgs){
+      // console.log(imgs);
+      createImage(imgs);
+    });
+    console.log(resImg);
+  } catch (err){
     console.error(err);
   }
 }
-loadNPause();
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
