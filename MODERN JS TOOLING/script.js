6a0078e77768d2c202ea622eb2e1da357652729a
Mapelujo <olamideiyanda18@gmail.com>
2024-01-15 12:46:23 +0100

    diving deeper into babel, transpiling & polyfilling

--- MODERN JS TOOLING/script.js
+++ MODERN JS TOOLING/script.js
@@ -10,10 +10,10 @@ console.log('Importing Module');
 // console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
 
 // import add, {addToCart, totalPrice as price, tq} from './shoppingCart.js'; // Import default export from shoppingCart.js
-// import add, { cart } from './shoppingCart.js'; // Import default export from shoppingCart.js
-// add('pizza', 2);
-// add('bread', 5);
-// add('apples', 15);
+import add, { cart } from './shoppingCart.js'; // Import default export from shoppingCart.js
+add('pizza', 2);
+add('bread', 5);
+add('apples', 15);
 
 // console.log(cart);
 
@@ -32,21 +32,49 @@ console.log('Importing Module');
 // const lastPost = getLastPost();
 // console.log(lastPost);
 
-// const lastPost2 = await getLastPost(); 
+// const lastPost2 = await getLastPost();
 // console.log(lastPost2);
 
-import clone from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from 'lodash-es';
 
 const state = {
-    cart: [
-        {product: 'bread', quantity: 5},
-        {product: 'pizza', quantity: 5},
-    ],
-    user: {loggedIn: true},
-}
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
 
 const stateClone = Object.assign({}, state);
-const stateDeepClone = clone(state);
+const stateDeepClone = cloneDeep(state);
 state.user.loggedIn = false;
 console.log(stateClone);
 console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person{
+    greeting = 'Hey';
+    constructor(name){
+        this.name = name;
+        console.log(`${this.greeting}, ${this.name}`);
+    }
+}
+
+const Lancer = new Person('Lancer');
+
+console.log('lancer' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+// import 'core-js/stable';
+import 'core-js/stable/array/find';
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime.js'
+
+
